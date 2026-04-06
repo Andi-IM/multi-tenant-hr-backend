832022a33370c8c3da355f5f2f9792f1a433671b
Andi-IM <andi.irhamm@gmail.com>
2026-04-06 15:12:54 +0700

    feat: add infrastructure configuration, Dockerfiles, Nginx template, and logger package for multi-tenant services

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,55 +1,62 @@
 variable "project_id" {
-  type = string
+  description = "The Google Cloud project ID"
+  type        = string
 }
 
 variable "region" {
-  type    = string
-  default = "asia-southeast2"
+  description = "The Google Cloud region"
+  type        = string
 }
 
 variable "artifact_registry_repository_id" {
-  type    = string
-  default = "mthrb"
-}
-
-variable "company_service_image" {
-  type = string
-}
-
-variable "attendance_service_image" {
-  type = string
-}
-
-variable "edge_gateway_image" {
-  type = string
-}
-
-variable "company_a_mongodb_uri" {
-  type      = string
-  sensitive = true
-}
-
-variable "company_b_mongodb_uri" {
-  type      = string
-  sensitive = true
-}
-
-variable "attendance_mongodb_uri" {
-  type      = string
-  sensitive = true
-}
-
-variable "timezone" {
-  type    = string
-  default = "Asia/Jakarta"
+  description = "The ID of the Artifact Registry repository"
+  type        = string
 }
 
 variable "cloud_run_min_instances" {
-  type    = number
-  default = 0
+  description = "Minimum number of Cloud Run instances"
+  type        = number
+  default     = 0
 }
 
 variable "cloud_run_max_instances" {
-  type    = number
-  default = 10
+  description = "Maximum number of Cloud Run instances"
+  type        = number
+  default     = 1
+}
+
+variable "company_service_image" {
+  description = "Docker image for the Company service"
+  type        = string
+}
+
+variable "timezone" {
+  description = "Timezone for the services"
+  type        = string
+  default     = "UTC"
+}
+
+variable "company_a_mongodb_uri" {
+  description = "MongoDB URI for Company A"
+  type        = string
+}
+
+variable "company_b_mongodb_uri" {
+  description = "MongoDB URI for Company B"
+  type        = string
+}
+
+variable "attendance_service_image" {
+  description = "Docker image for the Attendance service"
+  type        = string
+}
+
+variable "attendance_mongodb_uri" {
+  description = "MongoDB URI for the Attendance service"
+  type        = string
+}
+
+variable "edge_gateway_image" {
+  description = "Docker image for the Edge Gateway service"
+  type        = string
 }
